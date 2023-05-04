55c3b59a4a50541fd41db54896600de23f9ad166
lex0819 <lexmolnar@gmail.com>
2023-05-04 17:39:01 +0300

    hide left code

--- StudentMVCApp/src/App.java
+++ StudentMVCApp/src/App.java
@@ -17,19 +17,19 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        List<Student> students = new ArrayList<Student>();
-        Student s1 = new Student("Сергей", "Иванов", 21, (long)101);
-        Student s2 = new Student("Андрей", "Сидоров", 22, (long)111);
-        Student s3 = new Student("Иван", "Петров", 22, (long)121);
-        Student s4 = new Student("Игорь", "Иванов", 23, (long)301);
-        Student s5 = new Student("Даша", "Цветкова", 25, (long)171);
-        Student s6 = new Student("Лена", "Незабудкина", 23, (long)104);
-        students.add(s1);
-        students.add(s2);
-        students.add(s3);
-        students.add(s4);
-        students.add(s5);
-        students.add(s6);
+//        List<Student> students = new ArrayList<Student>();
+//        Student s1 = new Student("Сергей", "Иванов", 21, (long)101);
+//        Student s2 = new Student("Андрей", "Сидоров", 22, (long)111);
+//        Student s3 = new Student("Иван", "Петров", 22, (long)121);
+//        Student s4 = new Student("Игорь", "Иванов", 23, (long)301);
+//        Student s5 = new Student("Даша", "Цветкова", 25, (long)171);
+//        Student s6 = new Student("Лена", "Незабудкина", 23, (long)104);
+//        students.add(s1);
+//        students.add(s2);
+//        students.add(s3);
+//        students.add(s4);
+//        students.add(s5);
+//        students.add(s6);
 
 
         FileRepo fileRepo = new FileRepo("StudentDb.txt");
